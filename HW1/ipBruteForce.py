b7c216870b2e2ec49e385eb451f4b5bd8e2a1abf
Jenny <s110910516@student.nqu.edu.tw>
2022-09-28 09:54:55 +0800

    HW19.28.2

--- HW1/ipBruteForce.py
+++ HW1/ipBruteForce.py
@@ -43,7 +43,7 @@ for a in range(minall,maxall+1):
             for d in range(minall,maxall+1):
                 for e in range(minall,maxall+1):
                     if(5*a+7*b+9*c+2*d+1*e<=250&18*a+4*b-9*c+10*d+12*e<=285&4*a+7*b+3*c+8*d+5*e<=211&5*a+13*b+16*c+3*d-7*e<=315):
-                        ans=7*a+8*b+2*c+9*+6*e
+                        ans=7*a+8*b+2*c+9*d+6*e
                         if(ans>saveall):
                             saveall=ans
                             savea=a
@@ -51,6 +51,7 @@ for a in range(minall,maxall+1):
                             savec=c
                             saved=d
                             savee=e
+                            print(a,b,c,d,e,saveall)
 
 print(a,b,c,d,e,saveall)
 
